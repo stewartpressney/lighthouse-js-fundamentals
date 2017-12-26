368611cab291df39055b700114b8e5feb477eec6
Stewartpressney <stewartpressney@gmail.com>
2017-12-26 12:52:17 +0000

    add ageCalc.js

---
+++ ageCalc.js
@@ -0,0 +1,6 @@
+function ageCalcultor(name, yearOfBirth, currentYear) {
+  // body...
+  return name + " is " + (currentYear - yearOfBirth) + " years old.";
+}
+
+console.log(ageCalcultor("Stewrat", 1993, 2017));
